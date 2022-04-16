38757641b9dce24355cb0030de4eaf09fe5ac250
Md. Shakil Ahmed <ahmedshakil0512@gmail.com>
2022-04-17 05:20:53 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Header from './Shared/Header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Banner.css';
+
+
+const Banner = () => {
+    return (
+        <div className='banner-container'>
+            
+        </div>
+    );
+};
+
+export default Banner;
