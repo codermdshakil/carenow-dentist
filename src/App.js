1f82a621dc6b4dc9f731b777cc1412a5e4de6e88
Md. Shakil Ahmed <ahmedshakil0512@gmail.com>
2022-04-17 16:54:54 +0600

    show detaild page added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NotFound from './Shared/NotFound/NotFound';
 import Footer from './Shared/Footer/Footer';
 import Login from './Pages/Login/Login';
 import SignUp from './Pages/SignUp/SignUp';
+import ServiceDetails from './Pages/ServiceDetails/ServiceDetails';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/service/:serviceId' element={<ServiceDetails></ServiceDetails>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path='/signup' element={<SignUp></SignUp>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -2,11 +2,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Service.css';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 
 const Service = ({ product }) => {
 
-    const { img, name, price, description } = product;
+    const {id, img, name, price, description } = product;
+
+    const navigate = useNavigate()
+
+
+    const handleShowDetails = id => {
+        navigate(`/service/${id}`)
+    }
 
     return (
         <div className='col-lg-4 col-md-6 col-11 d-block m-auto m-md-0  '>
@@ -18,7 +26,7 @@ const Service = ({ product }) => {
                     <h3>{name}</h3>
                     <h4>${price}</h4>
                     <p>{description}</p>
-                    <button className='serviceDetail-btn' >Show Details <FontAwesomeIcon className='ms-2' icon={faArrowRight} /></button>
+                    <button onClick={() => handleShowDetails(id)}  className='serviceDetail-btn' >Show Details <FontAwesomeIcon className='ms-2' icon={faArrowRight} /></button>
                 </div>
             </div>
         </div>
