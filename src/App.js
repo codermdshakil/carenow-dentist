7075c0db0df2e454c7226103d55740692047f770
Md. Shakil Ahmed <ahmedshakil0512@gmail.com>
2022-04-18 03:48:30 +0600

    sign up form design

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './Pages/Home/Home';
 import Footer from './Shared/Footer/Footer';
 import NotFound from './Shared/NotFound/NotFound';
 import Login from './Pages/Login/Login';
+import SignUp from './Pages/SignUp/SignUp';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
