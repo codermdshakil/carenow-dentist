0ef3ab82dcf692d2268be31067508e4da9cb6260
Md. Shakil Ahmed <ahmedshakil0512@gmail.com>
2022-04-18 00:30:27 +0600

    create a react project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
 import './App.css';
-import Header from './Shared/Header/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home/Home';
-import NotFound from './Shared/NotFound/NotFound';
-import Footer from './Shared/Footer/Footer';
-import Login from './Pages/Login/Login';
-import SignUp from './Pages/SignUp/SignUp';
-import ServiceDetails from './Pages/ServiceDetails/ServiceDetails';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='/service/:serviceId' element={<ServiceDetails></ServiceDetails>}></Route>
-        <Route path="/login" element={<Login></Login>}></Route>
-        <Route path='/signup' element={<SignUp></SignUp>}></Route>
-        <Route path='*' element={<NotFound></NotFound>}></Route>
-      </Routes>
-      <Footer></Footer>
+     <h1>hello world</h1>
     </div>
   );
 }
