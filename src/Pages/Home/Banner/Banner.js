7665dabb81b52b18434a05cc3b535f4cefa490a9
Md. Shakil Ahmed <ahmedshakil0512@gmail.com>
2022-04-18 02:51:01 +0600

    some change

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Banner.css';
 import person1 from '../../../images/persons/person-2.png';
 
-
 const Banner = () => {
     return (
         <div className='banner-container' id='banner' >
@@ -13,7 +12,7 @@ const Banner = () => {
                         <p className='semi-title'>We use latest medical technology</p>
                         <h2 className='banner-title'>Your <span className='smile'> Smile </span> is <br /> Our Priority</h2>
                         <p>Dentists diagnose and treat problems with patients' teeth, gums, and related parts of the mouth. They provide advice and instruction on taking care of the teeth.</p>
-                        <button className='banner-btn' >Know More {/* <FontAwesomeIcon icon={faArrowCircleRight} /> */} </button>
+                        <button className='banner-btn' >Know More </button>
                     </div>
                 </div>
                 <div className="col-lg-6 col-md-6 col-11 d-block m-auto m-md-0  order-first order-md-last">
